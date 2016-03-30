3618ce0ab477762bf70fdfcf8eadac2d2748582c
Facundo Arias <farias@facunotebook.argentina.ibm.com>
2016-03-30 16:48:03 -0300

    Saqué la basura de ArrayList

--- src/Programa_Principal/Main.java
+++ src/Programa_Principal/Main.java
@@ -13,15 +13,5 @@ public class Main {
         CambioMoneda a= new CambioMoneda();
         a.calcularCambio(0.12f);
         System.out.println(a.toString());
-
-
-        ArrayList<User> users = new ArrayList<>();
-        User c = new User();
-        c.setNombre("Facundo");
-        c.setApellido("Arias");
-        c.setDni("3680891");
-        users.add(c);
-
-        users.forEach((v) -> System.out.printf("Nombre: %s\n",v.getNombre()));
     }
 }
